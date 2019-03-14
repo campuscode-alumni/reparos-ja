458dde9bce060f96cca5f42ee7e60cce79d19492
Luis Arantes <luis.arantes.sp@gmail.com>
2019-03-13 22:15:06 -0300

    Resolucao parcial testes criar conta Prestador
    Co-authored-by: Thais <thais.vnunes59@gmail.com>

--- spec/features/contractor_create_account_spec.rb
+++ spec/features/contractor_create_account_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 feature 'Contractor create account' do
   scenario 'successfully' do
     create(:category, name: 'Pedreiro')
-    
+
     visit root_path
     click_on 'Cadastrar-se como Prestador'
 
     fill_in 'Nome', with: 'João'
     fill_in 'E-mail', with: 'joao@campuscode.com.br'
     fill_in 'Senha', with: 'Joao123'
+    fill_in 'Confirmar Senha', with: 'Joao123'
     fill_in 'CPF', with: '12345678909'
     select 'Pedreiro', from: 'Categoria' 
     click_on 'Enviar'
