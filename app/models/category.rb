ef20d65efdd336b65f092dc004d69b918ad5bf69
L-Ghost <luis.arantes.sp@gmail.com>
2019-03-16 22:07:25 -0300

    Increment user edit category test
      adds new test that will not allow user to edit category leaving
    name blank; also adds test resolution

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   has_many :contractors
-
+  
+  validates :name, presence: {message: 'Nome não pode ser vazio'}
   validates :name, uniqueness: {message: 'Já existe uma categoria com este nome'}
 end
