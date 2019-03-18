e76e15d1d6e1fce82b6a359c131e9e2e1316d497
Laerte Guimarães <31661772+laerte-guimaraes@users.noreply.github.com>
2019-03-18 12:24:03 -0300

    Insere update para Contractors (#1)
    * Traduz criação de usuários
    
    * Insere update para Contractors

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -7,4 +7,21 @@ class ContractorsController < ApplicationController
   def show
     @contractor = Contractor.find(params[:id])
   end
+
+  def edit
+    @contractor = Contractor.find(params[:id])
+  end
+
+  def update
+    @contractor = Contractor.find(params[:id])
+    if @contractor.update(contractor_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def contractor_params
+    params.require(:contractor).permit(:name, :email)
+  end
 end
--- spec/factories/contractors.rb
+++ spec/factories/contractors.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :contractor do
     name { "MyString" }
+    email { 'mail@teste.com' }
     category
+    cpf { '1234567890' }
+    password { '123456' }
   end
 end
