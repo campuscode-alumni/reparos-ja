5cf827add0422299866f80289301ca1f3b11b18b
Laerte e Cadu <webmaster.kdu@gmail.com>
2019-03-21 20:09:20 -0300

    Insere tradução das telas prontas
    Co-authored-by: Laerte <laerte.guimaraes@live.com>

--- spec/features/visitor_create_user_account_spec.rb
+++ spec/features/visitor_create_user_account_spec.rb
@@ -6,11 +6,11 @@ feature 'visitor create user account' do
     visit root_path
     click_on 'Cadastrar-se como Usuário'
 
-    fill_in('Name', with: 'Teste User')
+    fill_in('Nome', with: 'Teste User')
     fill_in('Email', with: 'email@teste.com')
-    fill_in('Password', with: '123456')
-    fill_in('Confirmation Password', with: '123456')
-    click_on 'Sign up'
+    fill_in('Senha', with: '123456')
+    fill_in('Confirme sua senha', with: '123456')
+    click_on 'Criar Usuário'
 
     expect(page).to have_content('Bem vindo! Você realizou seu registro com sucesso.')
     expect(current_path).to eq(root_path)
@@ -21,11 +21,11 @@ feature 'visitor create user account' do
     visit root_path
     click_on 'Cadastrar-se como Usuário'
 
-    fill_in('Name', with: '')
+    fill_in('Nome', with: '')
     fill_in('Email', with: 'email@teste.com')
-    fill_in('Password', with: '123456')
-    fill_in('Confirmation Password', with: '123456')
-    click_on 'Sign up'
+    fill_in('Senha', with: '123456')
+    fill_in('Confirme sua senha', with: '123456')
+    click_on 'Criar Usuário'
 
     expect(page).to have_content('É necessário informar um nome')
     expect(current_path).to eq(user_registration_path)
